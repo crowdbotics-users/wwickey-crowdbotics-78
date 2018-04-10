7da52b9cd11cd45a3d6bed98bcb46c9355d41c95
Saurav <sp.gharti@gmail.com>
2018-04-10 17:45:25 +0000

    add package aa_airtable

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'aa_airtable', 'url': 'http://pypi.python.org/pypi/aa_airtable/0.2'},
     ]
     context = {
         'title': 'wwickey-crowdbotics-78',
